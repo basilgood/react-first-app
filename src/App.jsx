26696ec9c71ac1035c93b01347f73336b0cf8bfc
vasile <elsile69@yahoo.com>
2019-03-08 15:31:28 +0200

    Update configs and put some tests

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <div>
+        <span style={this.styles} className="badge badge-warning m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "ZERO" : count;
+  }
+}
+
+export default App;
